52fab1f471d7f24e13b0596339c9fb3242d47911
JeongHyeon-01 <auddwd19@naver.com>
2024-06-06 20:08:13 +0900

    배포실습

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import { useState } from 'react';
 function App() {
   const [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  const [titleIdx, setTitleIdx] = useState(0);
   const [likeCount, setLikeCount] = useState([0,0,0]);
   const [modal, setModal] = useState(false)
-  
-  function Modal() {
+  let [Ininput, setIninput] = useState('')
+  function Modal(props) {
     return (
       <div className='modal'>
-        <h4>제목</h4>
+        <h4>{props.title[props.idx]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button
+         onClick={()=>{
+          let copy = [...title]
+          copy[0] = '여자코트추천'
+          setTitle(copy)
+         }}
+        >글수정</button>
       </div>
     )
   }
+
 
   return (
     <div className="App">
@@ -37,75 +46,43 @@ function App() {
           <div className='list' key={i}>
             <h4 
             onClick={()=>{
-              let copy = [...likeCount];
-              copy[i] =  copy[i] +1;
-              setLikeCount(copy) 
+                setModal(modal === true ? false : true );
+                setTitleIdx(i);
               }}>
                 { title[i] }
-              <span>
+              <span
+              onClick={(e)=>{
+                e.stopPropagation();
+                let copy = [...likeCount];
+                  copy[i] =  copy[i] +1;
+              setLikeCount(copy) 
+              }}>
               👍 {likeCount[i]}
               </span>
+              <button 
+                onClick={(e)=>{
+                  e.stopPropagation();
+                  let copy = [...title]
+                  copy.splice(i,1)
+                  setTitle(copy)
+                }}
+              >삭제</button>
             </h4>
             <p>2월 18일 발행</p>
           </div>
         )
       })
      }
-     {/* <div className='list'>
-      <h4
-        onClick={()=>{
-          setModal(!modal)
-        }}
-      >{title[0]}
-        <span
-          onClick={()=>{
-            setLikeCount(likeCount+1)
-          }}
-        >
-            👍
-        </span>
-        { likeCount }
-        <span
-          onClick={()=>{
-            let copy = [...title];
-            console.log(title)
-            copy[0] = '여자 코트 추천'
-            setTitle(copy)
-          }}
-        >
-           ♀︎
-        </span>
-      </h4>
-      <p>2월 17일 발행</p>
-     </div>
-     <div className='list'>
-      <h4>{title[1]}
-        <span
-          onClick={()=>{
-            setLikeCount(likeCount+1)
-          }}
-        >
-            👍
-        </span>
-        { likeCount }
-      </h4>
-      <p>2월 17일 발행</p>
-     </div>
-     <div className='list'>
-      <h4>{title[2]}
-        <span
-          onClick={()=>{
-            setLikeCount(likeCount+1)
-          }}
-        >
-            👍
-        </span>
-        { likeCount }
-      </h4>
-      <p>2월 17일 발행</p>
-     </div> */}
-     {
-      modal == false ?  null : <Modal/> 
+      <input onChange={(e)=>{
+        setIninput(e.target.value)
+      }}/>
+      <button onClick={()=>{
+        let copy = [...title]
+        copy.unshift(Ininput)
+        setTitle(copy)
+      }}> 추가하기 </button>
+     {  
+      modal === true ?  <Modal title={title} idx={titleIdx} /> : null  
      }
     </div>
   );
